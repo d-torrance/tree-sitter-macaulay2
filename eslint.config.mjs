8496eb8d73630149168e3ae161b16ebfaf534c46
Doug Torrance <dtorrance@piedmont.edu>
2026-01-31 23:47:04 -0500

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import treesitter from 'eslint-config-treesitter';
+
+const treesitterConfigs = treesitter.map((cfg) => ({
+  ...cfg,
+  files: ['grammar.js'],
+}));
+
+export default defineConfig([
+  {
+    files: ['eslint.config.mjs'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
+  ...treesitterConfigs,
+  globalIgnores(['package-lock.json']),
+]);
